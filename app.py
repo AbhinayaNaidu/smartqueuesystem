1d114fe07a4401dc60d14a556821d47a2e577cfb
AbhinayaNaidu <naiduabhinaya006@gmail.com>
2026-03-12 20:57:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -112,5 +112,9 @@ def admin_update():
     conn.close()
     return "", 204
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
